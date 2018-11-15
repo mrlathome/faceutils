b4e0e6daffe970103c78a67ac79f60b7a8c74fe6
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2018-11-15 10:14:29 +0330

    first commit as new version, changed to faceutils

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="faceutils",
+    packages=['faceutils'],
+    version="1.0.1",
+    description="Real-Time face recognizer based on dlib.",
+    url="http://github.com/mrlathome/faceutils",
+    author="Ahmadreza Zibaei",
+    author_email="zibaeiahmadreza@gmail.com",
+    license='MIT',
+    zip_safe=False,
+    keywords=['dlib', 'image processing', 'opencv', 'face recognition'],    
+)
